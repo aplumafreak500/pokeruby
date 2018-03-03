071b45a2332575422098d1da2f53b085371a57ea
aplumafreak500 <aplumafreak500@gmail.com>
2018-03-03 02:11:38 -0500

    You know what? I think I'll just start from scratch.

--- sound/songs/se_w043.s
+++ sound/songs/se_w043.s
@@ -1,6 +1,6 @@
 	.include "MPlayDef.s"
 
-	.equ	se_w043_grp, voicegroup_869D6F4
+	.equ	se_w043_grp, voicegroup_8451F90
 	.equ	se_w043_pri, 4
 	.equ	se_w043_rev, reverb_set+50
 	.equ	se_w043_mvl, 127
