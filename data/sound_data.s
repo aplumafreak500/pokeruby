e35ef0eaed45edbe6bcfe4592893274097719ec1
aplumafreak500 <aplumafreak500@gmail.com>
2018-03-19 17:06:15 -0400

    sound_data.s

--- data/sound_data.s
+++ data/sound_data.s
@@ -4,10 +4,14 @@
 	.include "include/macros/music_voice.inc"
 
 	.include "sound/voice_groups.inc"
+	.include "sound/voice_groups_rs.inc"
 	.include "sound/keysplit_tables.inc"
+	.include "sound/keysplit_tables_rs.inc"
 	.include "sound/programmable_wave_data.inc"
+	.include "sound/programmable_wave_data_rs.inc"
 	.include "sound/music_player_table.inc"
 	.include "sound/song_table.inc"
 	.include "sound/direct_sound_data.inc"
+	.include "sound/direct_sound_data_rs.inc"
 
 	.align 2
