5c4148f802cf0b293f0d2fdd651c2d48e52fd57a
aplumafreak500 <aplumafreak500@gmail.com>
2018-07-13 15:56:34 -0400

    Add LCRNG constants from other Pokemon games

--- include/random.h
+++ include/random.h
@@ -5,9 +5,15 @@ extern u32 gRngValue;
 
 //Returns a 16-bit pseudorandom number
 u16 Random(void);
+u16 Random_XD(void);
+u16 Random_N64(void);
+u16 Random_Generation4(void);
 
 //Returns a 32-bit pseudorandom number
 #define Random32() (Random() | (Random() << 16))
+
+// Wishmaker Jirachi PID generation
+#define Random32_Swapped() (Random() << 16 | (Random()))
 
 //Sets the initial seed value of the pseudorandom number generator
 void SeedRng(u16 seed);
--- src/random.c
+++ src/random.c
@@ -12,6 +12,24 @@ u16 Random(void)
     return gRngValue >> 16;
 }
 
+u16 Random_XD(void)
+{
+    gRngValue = 214013 * gRngValue + 2531011;
+    return gRngValue >> 16;
+}
+
+u16 Random_N64(void)
+{
+    gRngValue = 1103515245 * gRngValue + 12347;
+    return gRngValue >> 16;
+}
+
+u16 Random_Generation4(void)
+{
+    gRngValue = 1812433253 * gRngValue + 1;
+    return gRngValue >> 16;
+}
+
 void SeedRng(u16 seed)
 {
     gRngValue = seed;
