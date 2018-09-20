8b0cf3278e64321b1d61e7557a72c7f76ae7d692
aplumafreak500 <aplumafreak500@gmail.com>
2018-09-20 17:01:12 -0400

    Slight oversight in var editor corrected, now should return a relative address

--- src/event_data.c
+++ src/event_data.c
@@ -113,7 +113,7 @@ bool32 CanResetRTC(void)
 u16 *GetVarPointer(u16 id)
 {
     if (id < VARS_START)
-        return &gSaveBlock1 - 0xcc0;
+        return &gSaveBlock1 - (0xcc0 - (id / 2));
     if (id < 0x8000)
         return &gSaveBlock1.vars[id - VARS_START];
     return gSpecialVars[id - 0x8000];
